ea163897bad18d5d94929fdc8da8d4aa00912bba
RealSchnedi <schnedi3@gmail.com>
2021-06-30 23:13:58 +0200

    Revert "sm8250-common: parts: Enable FOD service with prop"

--- kona.mk
+++ kona.mk
@@ -187,7 +187,6 @@ PRODUCT_PACKAGES += \
     fastbootd
 
 # Fingerprint
-ifeq ($(TARGET_HAS_FOD),true)
 PRODUCT_PACKAGES += \
     vendor.lineage.biometrics.fingerprint.inscreen@1.1-service.xiaomi_kona \
     FODService
@@ -197,7 +196,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     vendor/derp/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
-endif
 
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.xiaomi_kona
